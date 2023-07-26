0374394176accad8e76200e750b7332bcbb460e1
Roshni20-sys <132455744+Roshni20-sys@users.noreply.github.com>
2023-07-26 16:59:42 +0530

    Add files via upload

---
+++ Hello World.java
@@ -0,0 +1,17 @@
+class HellowWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hellow World");
+    }
+}
+
+class launch
+{
+    public static void main(String[] args)
+    {
+     System.out.println("My name is Roshni Hembrom.");
+     System.out.println("Iam from Kandi,Murshidabad.");
+     System.out.println("Iam a student of UIT, Burdwan");
+    }
+}
